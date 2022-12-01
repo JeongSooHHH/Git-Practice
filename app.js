6aacfe07b5c3d5e04769de8cbead60106ece53ee
JeongSooHHH <space2577@gmail.com>
2022-12-01 18:13:44 +0900

    [40기 황정수] Add: sign up feature

---
+++ app.js
@@ -0,0 +1,20 @@
+app.post("/users/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  return await myDataSource.query(
+    `
+      INSERT INTO
+        users (
+          username,
+          email,
+          password			
+        )
+      VALUES (
+        ?,
+        ?,
+        ?,
+        ?
+      )
+    `,
+    [username, email, password]
+  );
+});
